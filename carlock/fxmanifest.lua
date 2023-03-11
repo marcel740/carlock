f041bc960abeb7c7293d9d198de9e36b2f344074
marcel740 <126259820+marcel740@users.noreply.github.com>
2023-03-11 14:25:05 +0100

    upload carlock

---
+++ carlock/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'marcel_moncler'
+version 'alpha'
+description 'CARLOCK'
+
+client_scripts {'client.lua'}
+server_scripts {'server.lua', '@mysql-async/lib/MySQL.lua'}
